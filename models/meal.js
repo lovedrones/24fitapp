6a7035fa6c48bf859457ca725b362f796b4b3042
lovedrones <JuanTron@juans-mbp.attlocal.net>
2019-12-06 01:32:14 -0600

    more styling- table grid functioning

--- models/meal.js
+++ models/meal.js
@@ -4,11 +4,13 @@ var Schema = mongoose.Schema;
 
 var mealSchema = new Schema({
     mealType: { type: String,
-     enum: ['breakfast', 'lunch', 'dinner']},
+     enum: ['breakfast', 'lunch', 'dinner'] },
     meal: String,
     calories: Number,
     time: String,
-    date: { type: Date, default: Date.now }
+    day: { type: String,
+        enum: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    }
 })
 
 
